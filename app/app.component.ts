abbbfbc7e82f0649a9059efcf675b5f236844a33
tomhanlon <tomhanlon@gmail.com>
2015-12-15 18:50:51 -0600

    Fix typo

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,7 +2,7 @@ import {Component} from 'angular2/core';
 
 @Component({
   selector: 'my-app',
-  atemplate: '<h1>My First Angular 2 App</h1>'
+  template: '<h1>My First Angular 2 App</h1>'
 })
 
 export class AppComponent { }
